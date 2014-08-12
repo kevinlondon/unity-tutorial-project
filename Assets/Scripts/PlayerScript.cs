664ac79d8494322bf1955535cac11ce4bfe0e98b
kevinlondon <kevinlondon@gmail.com>
2014-08-11 22:22:48 -0700

    End of 'player-and-enemies'

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	// Summary:
+	// 1: The speed of the ship.
+	public Vector2 speed = new Vector2(50, 50);
+
+	// Store the movement.
+	public Vector2 movement;
+
+	// Update is called once per frame
+	void Update () {
+		// Retrieve axis info
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+
+		// Movement per direction
+		movement = new Vector2 (
+			speed.x * inputX,
+			speed.y * inputY);
+	}
+
+	void FixedUpdate() {
+		// Move the game object.
+		rigidbody2D.velocity = movement;
+	}
+}
